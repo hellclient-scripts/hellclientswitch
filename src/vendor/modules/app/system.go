32afab53ed1a11bbf1c520d9f079db6bc9bad0b0
jarlyyn <jarlyyn@qq.com>
2021-08-07 02:17:55 +0800

    update

--- src/vendor/modules/app/system.go
+++ src/vendor/modules/app/system.go
@@ -3,16 +3,19 @@ package app
 import (
 	"sync/atomic"
 
+	"github.com/herb-go/herbconfig/source"
 	"github.com/herb-go/util"
 	"github.com/herb-go/util/config"
 	"github.com/herb-go/util/config/tomlconfig"
-	"github.com/herb-go/herbconfig/source"
 )
 
 //SystemConfig system config data struct.
 //Struct must  unmarshaleable by Toml lib.
 //You should comment this struct if you use third party config struct.
 type SystemConfig struct {
+	Addr     string
+	Username string
+	Password string
 }
 
 //System config instance of system.
@@ -34,12 +37,11 @@ func (a *appSync) LoadSystem() *SystemConfig {
 	return v.(*SystemConfig)
 }
 
-
 func init() {
 	//Register loader which will be execute when Config.LoadAll func be called.
 	//You can put your init code after load.
 	//You must panic if any error rasied when init.
-	config.RegisterLoader(util.ConfigFile("/system.toml"),func(configpath source.Source) {
+	config.RegisterLoader(util.ConfigFile("/system.toml"), func(configpath source.Source) {
 		util.Must(tomlconfig.Load(configpath, System))
 		Sync.StoreSystem(System)
 	})
